a27b9c761f819f28cda0ed7eaae255cc54875ef0
unknown <ahnaf.ahmed.42173@gmail.com>
2023-12-11 21:02:03 +0600

    final

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,14 +1,13 @@
 import Banner from "../components/Banner";
 import EduAndExp from "../components/Edu&Exp/EduAndExp";
 import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
 
 const Home = () => {
     return (
-        <div className="max-w-7xl mx-auto">
-            <Navbar></Navbar>
+        <div className="max-w-7xl mx-auto mt-10">
+            {/* <Navbar></Navbar> */}
             <Banner></Banner>
             <Skills></Skills>
             <EduAndExp></EduAndExp>
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,6 +2,8 @@
 import { FaDownload } from "react-icons/fa6";
 import { MdMail } from "react-icons/md";
 
+import resume from '../assets/resume_ahnaf.pdf'
+
 import img from '../assets/resized-image.jpg'
 const Banner = () => {
     return (
@@ -12,7 +14,7 @@ const Banner = () => {
                     <h1 className="text-5xl font-bold">Hello, I'm <br />Ahnaf Ahmed</h1>
                     <p className="py-6">Frontend Developer with a Computer Science degree, ready to turn ideas into user-friendly designs. <br />I blend my academic knowledge with practical skills to create seamless digital experiences. <br /> Explore my portfolio for innovative solutions merging technology and creativity.</p>
                     {/* <button className="btn bg-neutral hover:bg-black text-white">Get My resume <FaDownload ></FaDownload> </button> */}
-                        <a className="btn bg-neutral hover:bg-black text-white" href="resume_ahnaf.pdf" download><FaDownload></FaDownload> Download Resume</a>
+                        <a className="btn bg-neutral hover:bg-black text-white" href={resume} download><FaDownload></FaDownload> Download Resume</a>
                         <a className="btn bg-neutral hover:bg-black text-white " href="mailto:ahnaf.ahmed.42173@gmail.com" download><MdMail></MdMail> Email Me</a>
 
 
