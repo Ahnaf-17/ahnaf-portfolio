90c1541ca6416df1f54070406270a63fc14383f3
unknown <ahnaf.ahmed.42173@gmail.com>
2024-01-10 11:41:17 +0600

    banner section modified

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -9,23 +9,22 @@ import { useEffect } from "react";
 import Aos from 'aos'
 import 'aos/dist/aos.css'
 const Banner = () => {
-    
-    useEffect(()=>{
-        Aos.init({duration: '1000'})
-    },[])
-    
+
+    useEffect(() => {
+        Aos.init({ duration: '1000' })
+    }, [])
+
     return (
         <div className="hero min-h-[500px] bg-base-content" >
             <div className="hero-content flex-col lg:flex-row-reverse text-white">
                 <div data-aos="fade-left">
-                <img src={img} className="max-w-sm rounded-lg " />
+                    <img src={img} className="max-w-sm rounded-lg " />
                 </div>
                 <div data-aos="fade-right">
                     <h1 className="text-5xl font-bold">Hello, I'm <br />Ahnaf Ahmed</h1>
                     <p className="py-6">Frontend Developer with a Computer Science degree, ready to turn ideas into user-friendly designs. <br />I blend my academic knowledge with practical skills to create seamless digital experiences. <br /> Explore my portfolio for innovative solutions merging technology and creativity.</p>
-                    {/* <button className="btn bg-neutral hover:bg-black text-white">Get My resume <FaDownload ></FaDownload> </button> */}
-                        <a className="btn bg-neutral hover:bg-black text-white" href={resume} download><FaDownload></FaDownload> Download Resume</a>
-                        <a className="btn bg-neutral hover:bg-black text-white " href="mailto:ahnaf.ahmed.42173@gmail.com" download><MdMail></MdMail> Email Me</a>
+                    <a className="btn bg-neutral hover:bg-black text-white mr-1" href={resume} download><FaDownload></FaDownload> Download Resume</a>
+                    <a className="btn bg-neutral hover:bg-black text-white" href="mailto:ahnaf.ahmed.42173@gmail.com" download><MdMail></MdMail> Email Me</a>
 
 
                 </div>
